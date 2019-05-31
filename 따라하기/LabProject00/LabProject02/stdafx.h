130d87e88d18f2754a62467e51d2ba2d774ff99a
jjangsu <39235624+jjangsu@users.noreply.github.com>
2019-05-31 23:56:45 +0900

    따라하기

---
+++ 따라하기/LabProject00/LabProject02/stdafx.h
@@ -0,0 +1,49 @@
+﻿// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
+// 표준 시스템 포함 파일 또는 프로젝트 특정 포함 파일이 들어 있는
+// 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // 거의 사용되지 않는 내용을 Windows 헤더에서 제외합니다.
+// Windows 헤더 파일
+#include <windows.h>
+
+// C 런타임 헤더 파일입니다.
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+
+// 여기서 프로그램에 필요한 추가 헤더를 참조합니다.
+
+#pragma comment(lib, "d3dcomplier")
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "dxgi.lib")
+
+#define FRAME_BUFFER_WIDTH 640
+#define FRAME_BUFFER_HEIGHT 480
+
+#include "stdafx.h"
+
+#include <string>
+#include <wrl.h>
+#include <shellapi.h>
+
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#include <d3dcompiler.h>
+
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+using Microsoft::WRL::ComPtr;
+
